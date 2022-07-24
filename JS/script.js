ff52aa523029d5c5b646c108568ca5b1715d1ad9
nHur2004 <nathan.hur04@gmail.com>
2022-07-24 12:20:08 -0700

    added functionality to character prompt, and finishing touches: '

--- JS/script.js
+++ JS/script.js
@@ -7,17 +7,20 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  // passwordText.value = password;
 
 };
 
 var generatePassword = function() {
-  var promptCriteria = window.prompt("Which criteria would you like included in your password? Type LENGTH to choose a distinct length. Type CHAR to choose character types. Type BOTH to choose both or type RANDOM for a fully random password.");
+  var promptCriteria = window.prompt("Which criteria would you like included in your password? \nType LENGTH to choose a distinct length. \nType CHAR to choose character types. \nOr type RANDOM for a fully random password.");
 
   // if field is empty, re-run the command
-  if (promptCriteria === "" || promptCriteria === null) {
+  if (promptCriteria === "") {
     window.alert("Choice was invalid, please try again.");
     return generatePassword();
+  } else if (promptCriteria === null) {
+    window.alert("Cancelled.");
+    stop;
   }
 
   promptCriteria = promptCriteria.toLocaleLowerCase();
@@ -29,9 +32,6 @@ var generatePassword = function() {
   // chooses only the character type criteria
   } else if (promptCriteria === "char") {
     charChooser();
-  // chooses both criteria choosers
-  } else if (promptCriteria === "both") {
-    bothChooser();
   // chooses a fully random password
   } else if (promptCriteria === "random") {
     randomRandom();
@@ -47,17 +47,50 @@ var randomLength = function() {
   return rndmLength;
 };
 
+// character choice code
+var charChoice = function(C) {
+  var charSelected = "";
+  var charULN = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+  var charUL = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  var charUN = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+  var charLS = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()";
+  if (C === "uln") {
+    for (i = 0; i < charULN.length; i++) {
+      charSelected += charULN.charAt(Math.floor(Math.random() * charULN.length));
+    }
+    return charSelected;
+  } else if (C === "ul") {
+    for (i = 0; i < charUL.length; i++) {
+      charSelected += charUL.charAt(Math.floor(Math.random() * charUL.length));
+    }
+    return charSelected;
+  } else if (C === "un") {
+    for (i = 0; i < charUN.length; i++) {
+      charSelected += charUN.charAt(Math.floor(Math.random() * charUN.length));
+    }
+    return charSelected;
+  } else if (C === "ls") {
+    for (i = 0; i < charLS.length; i++) {
+      charSelected += charLS.charAt(Math.floor(Math.random() * charLS.length));
+    }
+    return charSelected;
+  }
+};
+
 // random character code
-var randomRandom = function(L) {  
+var randomChar = function(length) {  
   var rndmChar = "";
   var charRandom = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()";
   var charRandomLength = charRandom.length;
-  for ( i = 0; i < randomLength(); i++ ) {
+  for ( i = 0; i < length; i++ ) {
     rndmChar += charRandom.charAt(Math.floor(Math.random() * charRandomLength));
   }
   return rndmChar;
 };
-console.log(randomRandom());
+
+var randomRandom = function() {
+  document.getElementById("password").value = randomChar(randomLength());
+}
 
 // length code
 var lengthChooser = function() {
@@ -68,6 +101,9 @@ var lengthChooser = function() {
   } // if the length value is between 8 and 128, continue
   if (length >= 8 && length <= 128) {
     window.alert("Your password will be " + length + " characters long.");
+
+    document.getElementById("password").value = randomChar(length);
+
     return length;
   } // if the length value is not between 8 and 128, ask again
   else {
@@ -75,6 +111,47 @@ var lengthChooser = function() {
     lengthChooser();
   }
 };
+
+// character code
+var charChooser = function() {
+  var charSelected = prompt("What type of characters would you like in your password?\nType ULNS for uppercase, lowercase, numbers, and symbols.\nType ULN for uppercase, lowercase, and numbers.\nType UL for uppercase and lowercase.\nType UN for uppercase and numbers.\nType LS for lowercase and symbols.");
+  
+  // lots of choices: ULNS ULN UL UN LS
+  charSelected.toLowerCase();
+  if (charSelected === "ulns") { // runs function to include all characters
+    window.alert("Your password will be a random length with all character types.");
+
+    document.getElementById("password").value = randomChar(randomLength());
+    return charSelected;
+  } else if (charSelected === "uln") { // runs function to run no symbols
+    window.alert("Your password will be a random length with no symbol character types.")
+
+    document.getElementById("password").value = charChoice("uln");
+    return charSelected;
+  } else if (charSelected === "ul") { // runs function to run upper and lowercase
+    window.alert("Your password will be a random length with upper and lowercase character types.")
+
+    document.getElementById("password").value = charChoice("ul");
+    return charSelected;
+  } else if (charSelected === "un") { // runs function to run upper and numbers
+    window.alert("Your password will be a random length with uppercase and number character types.")
+
+    document.getElementById("password").value = charChoice("un");
+    return charSelected;
+  } else if (charSelected === "ls") { // runs function to run lower and symbols
+    window.alert("Your password will be a random length with lowercase and symbol character types.")
+
+    document.getElementById("password").value = charChoice("ls");
+    return charSelected;
+  } else {
+    window.alert("Please input one of the 5 choices.");
+
+    charChooser();
+  } if (charSelected === null || charSelected === "null") {
+    window.alert("Cancelled.");
+    stop;
+  }
+}
 
 
 // Add event listener to generate button
